58cca6afc237e352088048f8b662c551f732f2fc
Wojtas059 <wojtas.maj34@gmail.com>
2022-02-18 14:43:14 +0100

    Change file client.py

--- src/grpc/python_client_server_dir/client_base_station_com/client.py
+++ src/grpc/python_client_server_dir/client_base_station_com/client.py
@@ -11,16 +11,14 @@ import src.grpc.protos_dir.protos_base_station_com.client_base_station_pb2 as Se
 import src.grpc.protos_dir.protos_base_station_com.client_base_station_pb2_grpc as Servicer
 
 
-class Client:
-    pass
-
 
 def main():
     channel = grpc.insecure_channel("192.168.1.107:50051")
     stub = Servicer.ClientBaseStationStub(channel)
-    response = stub.checkConnection(ServicerMethods.CheckConnection(name="client"))
-    print("Greeter client received: " + response.message)
+    response = stub.checkConnection(ServicerMethods.CheckConnection(stats="client"))
+    print("Greeter client received: "+ response.stats)
 
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     logging.basicConfig()
+    main()
