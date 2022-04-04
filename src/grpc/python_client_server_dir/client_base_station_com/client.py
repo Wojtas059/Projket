287137aa79c96bffa27b8b5caca7c2bf1072bfcb
Wojtas059 <wojtas.maj34@gmail.com>
2022-04-04 12:59:20 +0200

    Change grpc/client address ip

--- src/grpc/python_client_server_dir/client_base_station_com/client.py
+++ src/grpc/python_client_server_dir/client_base_station_com/client.py
@@ -13,7 +13,7 @@ class Client:
         self.channel = None
         self.stub = None
         self.transfer_status = False
-        self.channel = grpc.insecure_channel("192.168.1.107:50051")
+        self.channel = grpc.insecure_channel("192.168.1.103:50051")
         self.stub = Servicer.ClientBaseStationStub(self.channel)
 
     def connect(self):
